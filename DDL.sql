e34c25d9ce33728a049d51d9df84879efe0dc52a
Josesolano258 <133874269+Josesolano258@users.noreply.github.com>
2025-07-18 11:43:21 -0500

    second commit

--- DDL.sql
+++ DDL.sql
@@ -1,18 +1,18 @@
-CREATE TABLE sakilacampus.pais (
+CREATE TABLE pais (
     id_pais SMALLINT UNSIGNED PRIMARY KEY,
     nombre VARCHAR(50),
     ultima_actualizacion TIMESTAMP
 );
 
-CREATE TABLE sakilacampus.ciudad (
+CREATE TABLE ciudad (
     id_ciudad SMALLINT UNSIGNED PRIMARY KEY,
     nombre VARCHAR(50),
     id_pais SMALLINT UNSIGNED,
     ultima_actualizacion TIMESTAMP,
-    FOREIGN KEY (id_pais) REFERENCES sakilacampus.pais(id_pais)
+    FOREIGN KEY (id_pais) REFERENCES pais(id_pais)
 );
 
-CREATE TABLE sakilacampus.direccion (
+CREATE TABLE direccion (
     id_direccion SMALLINT UNSIGNED PRIMARY KEY,
     direccion VARCHAR(50),
     direccion2 VARCHAR(50),
@@ -21,39 +21,40 @@ CREATE TABLE sakilacampus.direccion (
     codigo_postal VARCHAR(10),
     telefono VARCHAR(20),
     ultima_actualizacion TIMESTAMP,
-    FOREIGN KEY (id_ciudad) REFERENCES sakilacampus.ciudad(id_ciudad)
+    FOREIGN KEY (id_ciudad) REFERENCES ciudad(id_ciudad)
 );
 
-CREATE TABLE sakilacampus.idioma (
+CREATE TABLE idioma (
     id_idioma TINYINT UNSIGNED PRIMARY KEY,
     nombre CHAR(20),
     ultima_actualizacion TIMESTAMP
 );
 
-CREATE TABLE sakilacampus.empleado (
+CREATE TABLE empleado (
     id_empleado TINYINT UNSIGNED PRIMARY KEY,
     nombre VARCHAR(45),
     apellidos VARCHAR(45),
     id_direccion SMALLINT UNSIGNED,
+    imagen BLOB,
     email VARCHAR(50),
     id_almacen TINYINT UNSIGNED,
     activo TINYINT(1),
     username VARCHAR(16),
     password VARCHAR(40),
     ultima_actualizacion TIMESTAMP,
-    FOREIGN KEY (id_direccion) REFERENCES sakilacampus.direccion(id_direccion)
+    FOREIGN KEY (id_direccion) REFERENCES direccion(id_direccion)
 );
 
-CREATE TABLE sakilacampus.almacen (
+CREATE TABLE almacen (
     id_almacen TINYINT UNSIGNED PRIMARY KEY,
     id_empleado_jefe TINYINT UNSIGNED,
     id_direccion SMALLINT UNSIGNED,
     ultima_actualizacion TIMESTAMP,
-    FOREIGN KEY (id_empleado_jefe) REFERENCES sakilacampus.empleado(id_empleado),
-    FOREIGN KEY (id_direccion) REFERENCES sakilacampus.direccion(id_direccion)
+    FOREIGN KEY (id_empleado_jefe) REFERENCES empleado(id_empleado),
+    FOREIGN KEY (id_direccion) REFERENCES direccion(id_direccion)
 );
 
-CREATE TABLE sakilacampus.cliente (
+CREATE TABLE cliente (
     id_cliente SMALLINT UNSIGNED PRIMARY KEY,
     id_almacen TINYINT UNSIGNED,
     nombre VARCHAR(45),
@@ -63,24 +64,24 @@ CREATE TABLE sakilacampus.cliente (
     activo TINYINT(1),
     fecha_creacion DATETIME,
     ultima_actualizacion TIMESTAMP,
-    FOREIGN KEY (id_almacen) REFERENCES sakilacampus.almacen(id_almacen),
-    FOREIGN KEY (id_direccion) REFERENCES sakilacampus.direccion(id_direccion)
+    FOREIGN KEY (id_almacen) REFERENCES almacen(id_almacen),
+    FOREIGN KEY (id_direccion) REFERENCES direccion(id_direccion)
 );
 
-CREATE TABLE sakilacampus.actor (
+CREATE TABLE actor (
     id_actor SMALLINT UNSIGNED PRIMARY KEY,
     nombre VARCHAR(45),
     apellidos VARCHAR(45),
     ultima_actualizacion TIMESTAMP
 );
 
-CREATE TABLE sakilacampus.categoria (
+CREATE TABLE categoria (
     id_categoria TINYINT UNSIGNED PRIMARY KEY,
     nombre VARCHAR(25),
     ultima_actualizacion TIMESTAMP
 );
 
-CREATE TABLE sakilacampus.pelicula (
+CREATE TABLE pelicula (
     id_pelicula SMALLINT UNSIGNED PRIMARY KEY,
     titulo VARCHAR(255),
     descripcion TEXT,
@@ -94,38 +95,38 @@ CREATE TABLE sakilacampus.pelicula (
     clasificacion ENUM('G','PG','PG-13','R','NC-17'),
     caracteristicas_especiales SET('Trailers','Commentaries','Deleted Scenes','Behind the Scenes'),
     ultima_actualizacion TIMESTAMP,
-    FOREIGN KEY (id_idioma) REFERENCES sakilacampus.idioma(id_idioma),
-    FOREIGN KEY (id_idioma_original) REFERENCES sakilacampus.idioma(id_idioma)
+    FOREIGN KEY (id_idioma) REFERENCES idioma(id_idioma),
+    FOREIGN KEY (id_idioma_original) REFERENCES idioma(id_idioma)
 );
 
-CREATE TABLE sakilacampus.inventario (
+CREATE TABLE inventario (
     id_inventario MEDIUMINT UNSIGNED PRIMARY KEY,
     id_pelicula SMALLINT UNSIGNED,
     id_almacen TINYINT UNSIGNED,
     ultima_actualizacion TIMESTAMP,
-    FOREIGN KEY (id_pelicula) REFERENCES sakilacampus.pelicula(id_pelicula),
-    FOREIGN KEY (id_almacen) REFERENCES sakilacampus.almacen(id_almacen)
+    FOREIGN KEY (id_pelicula) REFERENCES pelicula(id_pelicula),
+    FOREIGN KEY (id_almacen) REFERENCES almacen(id_almacen)
 );
-
-CREATE TABLE sakilacampus.pelicula_actor (
+ 
+CREATE TABLE pelicula_actor (
     id_pelicula SMALLINT UNSIGNED,
     id_actor SMALLINT UNSIGNED,
     ultima_actualizacion TIMESTAMP,
     PRIMARY KEY (id_pelicula, id_actor),
-    FOREIGN KEY (id_pelicula) REFERENCES sakilacampus.pelicula(id_pelicula),
-    FOREIGN KEY (id_actor) REFERENCES sakilacampus.actor(id_actor)
+    FOREIGN KEY (id_pelicula) REFERENCES pelicula(id_pelicula),
+    FOREIGN KEY (id_actor) REFERENCES actor(id_actor)
 );
 
-CREATE TABLE sakilacampus.pelicula_categoria (
+CREATE TABLE pelicula_categoria (
     id_pelicula SMALLINT UNSIGNED,
     id_categoria TINYINT UNSIGNED,
     ultima_actualizacion TIMESTAMP,
     PRIMARY KEY (id_pelicula, id_categoria),
-    FOREIGN KEY (id_pelicula) REFERENCES sakilacampus.pelicula(id_pelicula),
-    FOREIGN KEY (id_categoria) REFERENCES sakilacampus.categoria(id_categoria)
+    FOREIGN KEY (id_pelicula) REFERENCES pelicula(id_pelicula),
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria)
 );
 
-CREATE TABLE sakilacampus.alquiler (
+CREATE TABLE alquiler (
     id_alquiler INT PRIMARY KEY,
     fecha_alquiler DATETIME,
     id_inventario MEDIUMINT UNSIGNED,
@@ -133,12 +134,12 @@ CREATE TABLE sakilacampus.alquiler (
     fecha_devolucion DATETIME,
     id_empleado TINYINT UNSIGNED,
     ultima_actualizacion TIMESTAMP,
-    FOREIGN KEY (id_inventario) REFERENCES sakilacampus.inventario(id_inventario),
-    FOREIGN KEY (id_cliente) REFERENCES sakilacampus.cliente(id_cliente),
-    FOREIGN KEY (id_empleado) REFERENCES sakilacampus.empleado(id_empleado)
+    FOREIGN KEY (id_inventario) REFERENCES inventario(id_inventario),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado)
 );
 
-CREATE TABLE sakilacampus.pago (
+CREATE TABLE pago (
     id_pago SMALLINT UNSIGNED PRIMARY KEY,
     id_cliente SMALLINT UNSIGNED,
     id_empleado TINYINT UNSIGNED,
@@ -146,12 +147,12 @@ CREATE TABLE sakilacampus.pago (
     total DECIMAL(5,2),
     fecha_pago DATETIME,
     ultima_actualizacion TIMESTAMP,
-    FOREIGN KEY (id_cliente) REFERENCES sakilacampus.cliente(id_cliente),
-    FOREIGN KEY (id_empleado) REFERENCES sakilacampus.empleado(id_empleado),
-    FOREIGN KEY (id_alquiler) REFERENCES sakilacampus.alquiler(id_alquiler)
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado),
+    FOREIGN KEY (id_alquiler) REFERENCES alquiler(id_alquiler)
 );
 
-CREATE TABLE sakilacampus.film_text (
+CREATE TABLE film_text (
     film_id SMALLINT PRIMARY KEY,
     title VARCHAR(255),
     description TEXT
